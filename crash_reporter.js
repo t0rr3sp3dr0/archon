c6e1ce38a3ce7af8944293ba97904601784f9fd3
vladikoff <vlad.filippov@gmail.com>
2014-09-21 21:50:38 -0400

    ARM.

--- crash_reporter.js
+++ crash_reporter.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd2918dcadfc039317f5eb10c5aa0b2eb0f3cdcaee91e0b24d1ff982520c2fde
-size 9046
+oid sha256:d41f82aebf628579eca8a53e2ed6512bf159e6ca56373f26a2595ba9cbaa28ce
+size 9107
